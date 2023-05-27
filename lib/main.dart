20460d013d5cbe5e51d1d4b1c630efbf3d7b1988
yosi <yosihofman21@gmail.com>
2023-05-28 01:27:27 +0300

    icon

--- lib/main.dart
+++ lib/main.dart
@@ -22,8 +22,13 @@ class Home extends StatelessWidget {
         //     fontFamily: 'IndieFlower'
         //   ),
         // ),
-        child: Image.network('https://images.unsplash.com/photo-1505506874110-6a7a69069a08?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80'),
-        // child: Image.asset('assets/space-1'),
+        // child: Image.network('https://images.unsplash.com/photo-1505506874110-6a7a69069a08?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80'),
+        // child: Image.asset('assets/space-1.jpg'),
+        child: Icon(
+          Icons.airport_shuttle,
+          color: Colors.lightBlue,
+        ),
+
 
    
       
