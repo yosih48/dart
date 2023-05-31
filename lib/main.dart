5e5f8cdd6c160d2846e468e78d02bc0cd49a3a5e
yosi <yosihofman21@gmail.com>
2023-05-31 22:11:05 +0300

    lesson4

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
-import 'package:my_project/screen/HomePage.dart';
+// import 'package:my_project/screen/HomePage.dart';
 // import 'package:my_project/screen/HomePageLogin.dart';
+import 'package:my_project/screen/lesson4.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,20 +15,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+
         primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue)
       ),
-      home: const MyHomePage(title: 'L ', number: 52),
+      // home: const MyHomePage(title: 'Login page ', number: 52),
+      home: const MyWidget(),
     );
   }
 }
---
+++ lib/screen/lesson4.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('appbar'),
+      ),
+
+      body: Column(
+        children: [
+          Container(
+            width: 200,
+            height: 200,
+            decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(75),
+              color: Colors.yellow,
+              border: Border.all(
+          color: Colors.red,
+              ),
+            ),
+            
+          ),
+
+               ElevatedButton(
+           
+            onPressed: () {},
+            child:  Text('Enabled'),
+          ),
+        ],
+      ),
+
+      drawer: Drawer(
+  // Add a ListView to the drawer. This ensures the user can scroll
+  // through the options in the drawer if there isn't enough vertical
+  // space to fit everything.
+  child: ListView(
+    // Important: Remove any padding from the ListView.
+    padding: EdgeInsets.zero,
+    children: [
+      const DrawerHeader(
+        decoration: BoxDecoration(
+          color: Colors.blue,
+        ),
+        child: Text('Drawer Header'),
+      ),
+      ListTile(
+        title: const Text('Item 1'),
+        onTap: () {
+          // Update the state of the app.
+          // ...
+        },
+      ),
+      ListTile(
+        title: const Text('Item 2'),
+        onTap: () {
+          // Update the state of the app.
+          // ...
+        },
+      ),
+    ],
+  ),
+),
+
+    );
+  }
+}
