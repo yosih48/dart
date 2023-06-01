e52ecf2ffd6e2b4d821bbb0c477ab3495b3e8e5b
yosi <yosihofman21@gmail.com>
2023-06-01 09:39:22 +0300

    widget functions

--- lib/screen/lesson4.dart
+++ lib/screen/lesson4.dart
@@ -14,62 +14,118 @@ class _MyWidgetState extends State<MyWidget> {
       appBar: AppBar(
         title: Text('appbar'),
       ),
-
       body: Column(
-        children: [
-          Container(
-            width: 200,
-            height: 200,
-            decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(75),
-              color: Colors.yellow,
-              border: Border.all(
-          color: Colors.red,
-              ),
-            ),
-            
-          ),
-
-               ElevatedButton(
-           
-            onPressed: () {},
-            child:  Text('Enabled'),
-          ),
-        ],
+        children: quotes.map((q) => cardFunc()).toList(),
       ),
-
       drawer: Drawer(
-  // Add a ListView to the drawer. This ensures the user can scroll
-  // through the options in the drawer if there isn't enough vertical
-  // space to fit everything.
-  child: ListView(
-    // Important: Remove any padding from the ListView.
-    padding: EdgeInsets.zero,
-    children: [
-      const DrawerHeader(
-        decoration: BoxDecoration(
-          color: Colors.blue,
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Drawer Header'),
+            ),
+            ListTile(
+              title: const Text('Item 1'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+            ListTile(
+              title: const Text('Item 2'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+          ],
         ),
-        child: Text('Drawer Header'),
       ),
-      ListTile(
-        title: const Text('Item 1'),
-        onTap: () {
-          // Update the state of the app.
-          // ...
-        },
-      ),
-      ListTile(
-        title: const Text('Item 2'),
-        onTap: () {
-          // Update the state of the app.
-          // ...
-        },
-      ),
-    ],
-  ),
-),
-
     );
   }
 }
+
+Widget test() {
+  return Column(
+    children: [
+      Container(
+        width: 200,
+        height: 200,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(75),
+          color: Colors.yellow,
+          border: Border.all(
+            color: Colors.red,
+          ),
+        ),
+      ),
+      ElevatedButton(
+        onPressed: () {},
+        child: Text('Enabled'),
+      ),
+    ],
+  );
+}
+
+Widget cardFunc() {
+  return Column(
+    children: [
+      Card(
+        elevation: 70,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const ListTile(
+              leading: Icon(Icons.album),
+              title: Text('The Enchanted Nightingale'),
+              subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                TextButton(
+                  child: const Text('BUY TICKETS'),
+                  onPressed: () {/* ... */},
+                ),
+                const SizedBox(width: 8),
+                TextButton(
+                  child: const Text('LISTEN'),
+                  onPressed: () {/* ... */},
+                ),
+                const SizedBox(width: 8),
+              ],
+            ),
+          ],
+        ),
+      )
+    ],
+  );
+}
+
+Widget listView() {
+  return Column(
+    children: [
+      Expanded(
+        child: ListView(
+          children: [
+            const Text('hi'),
+            IconButton(onPressed: () {}, icon: Icon(Icons.balance))
+          ],
+        ),
+      )
+    ],
+  );
+}
+
+List<String> quotes = [
+  'dsdsdsdsd',
+  'dsdsdsdsdds',
+  'בבבבבבבבבבבב',
+];
---
+++ lib/screen/screen2.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class screen2 extends StatelessWidget {
+  const screen2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+TextField(),
+// ElevatedButton(onPressed: {Navigator.pop(context);}, child: null)
+
+        ]
+      ),
+    );
+  }
+}
